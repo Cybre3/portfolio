02f3dc1045ecee8eb0e81b14ebfd171017fbc611
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2022-02-08 22:19:56 -0500

    intro created

---
+++ src/Components/Intro.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Typical from "react-typical";
+
+function Intro(props) {
+  return (
+    <div className="intro">
+      <div className="introBlock">
+        <h1>Hello, </h1>
+        <h1>
+          I am <u className="underline">Star</u>rika McCloud
+        </h1>
+        <div className="typical">
+          <h2>
+            {" "}
+            <Typical
+              loop={Infinity}
+              steps={[
+                "Full Stack Developer",
+                2000,
+                "MERN Stack Developer",
+                2000,
+                "Programmer",
+                2000,
+                "Troubleshooter",
+                2000,
+              ]}
+            />
+          </h2>
+        </div>
+      </div>
+      <div className="introBlock">
+        <a href="#/" className="resumeButton">
+          See My Resume
+        </a>
+        <a href="#/" className="resumeButton">
+          Download my Resume
+        </a>
+      </div>
+      <h3>
+        <i>Thank you for visiting my page</i>
+      </h3>
+    </div>
+  );
+}
+
+export default Intro;
