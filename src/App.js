027f9b345215b4a682b70bdad8c7e705bcd8dc08
Cybre3 <71160954+Cybre3@users.noreply.github.com>
2022-02-10 21:29:50 -0500

    added links and style to nav

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import Intro from "./Components/Intro";
 import Nav from "./Components/Nav";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import { Route, Routes, useLocation } from "react-router";
+// import { useEffect } from "react";
 
 function App() {
+  // const { pathname, hash, key } = useLocation();
+  // useEffect(() => {
+  //   // if not a hash link, scroll to top
+  //   if (hash === "") {
+  //     window.scrollTo(0, 0);
+  //   }
+  //   // else scroll to id
+  //   else {
+  //     setTimeout(() => {
+  //       const id = hash.replace("#", "");
+  //       const element = document.getElementById(id);
+  //       if (element) {
+  //         element.scrollIntoView();
+  //       }
+  //     }, 0);
+  //   }
+  // }, [pathname, hash, key]); // do this on route change
   return (
+    // <Router>
     <>
       <Nav />
       <Intro />
@@ -11,3 +32,8 @@ function App() {
 }
 
 export default App;
+/* 
+ {/* <Routes>
+        <Route exact path="/" component={Intro}></Route>
+      </Routes>
+    </Router> */
