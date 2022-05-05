145c434066d461ae9da6c481560fe4499912e8a5
Cybre <cybre3@gmail.com>
2022-05-05 18:13:43 -0500

    revamp on home container

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
-import CodingChallenges from "./Components/CodingChallenges";
-import Footer from "./Components/Footer";
-import Intro from "./Components/Intro";
-import Nav from "./Components/Nav";
-import ProjectContainer from "./Components/ProjectContainer";
-// import { BrowserRouter as Router } from "react-router-dom";
-// import { Route, Routes, useLocation } from "react-router";
-// import { useEffect } from "react";
+import PortfolioConatiner from "./PortfolioContainer/PortfolioConatiner";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import "./App.css";
 
 function App() {
-  // const { pathname, hash, key } = useLocation();
-  // useEffect(() => {
-  //   // if not a hash link, scroll to top
-  //   if (hash === "") {
-  //     window.scrollTo(0, 0);
-  //   }
-  //   // else scroll to id
-  //   else {
-  //     setTimeout(() => {
-  //       const id = hash.replace("#", "");
-  //       const element = document.getElementById(id);
-  //       if (element) {
-  //         element.scrollIntoView();
-  //       }
-  //     }, 0);
-  //   }
-  // }, [pathname, hash, key]); // do this on route change
+
   return (
-    // <Router>
-    <>
-      <Nav />
-      <Intro />
-      <ProjectContainer />     
-      <CodingChallenges />
-      <Footer />
-    </>
+    <div className="App">
+      <ToastContainer />
+      <PortfolioConatiner />
+    </div>
   );
 }
 
 export default App;
-/* 
- {/* <Routes>
-        <Route exact path="/" component={Intro}></Route>
-      </Routes>
-    </Router> */
