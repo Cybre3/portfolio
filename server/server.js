a2e0f925ff84ff4c6369bc99042ea3756ef11329
Cybre <cybre3@gmail.com>
2022-05-27 21:56:39 -0500

    added static path server logic

--- server/server.js
+++ server/server.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(express.json())
 app.use(cors())
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve("client", "build", "index.html"))
+  );
+}
+
 app.use("/", contactRoute);
 
 const port = process.env.PORT || 5000;
